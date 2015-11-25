b2617a13d474308cc9088e36f007f70273cdf3b6
Sagar Atalatti <sagar.atalatti@gmail.com>
2015-11-25 17:32:36 +0530

    Sms login created.

--- app/src/main/java/com/example/android/rabbit/CameraFragment.java
+++ app/src/main/java/com/example/android/rabbit/CameraFragment.java
@@ -1,15 +1,34 @@
 package com.example.android.rabbit;
 
+import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 /**
  * Created by sagar_000 on 11/12/2015.
  */
-public class CameraFragment extends ListFragment {
+public class CameraFragment extends Fragment {
+
+    //Activity request codes
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    private static final int CAMER_CAPTURE_VIDEO_REQUEST_CODE = 200;
+    private static final int MEDIA_TYPE_IMAGE = 1;
+    private static final int MEDIA_TYPE_VIDEO = 2;
+
+    //directory name to store captured images and video
+    private static final String IMAGE_DIRECTORY_NAME = "Rabbit";
+
+    private Uri fileUri; //file url to store image/video
+
+    private ImageView imgPreview;
+    private VideoView videoPreview;
+    private Button btnCapturePicture, btnRecordeVideo;
 
     //Copied from MainActivity
     @Override
@@ -17,5 +36,12 @@ public class CameraFragment extends ListFragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
         return rootView;
+
+        imgPreview =(ImageView) findViewById(R.id.imgPreview);
+        videoPreview = (VideoView) findViewById(R.id.videoPreview);
+        btnCapturePicture = (Button) findViewbyId(R.id.button);
+        btnRecordeVideo = (Button) findViewById(R.id.button)
     }
+
+
 }
